ca4774ebc0e6a29d1d22d1a998ca7b0fbdfe2d98
Jim Kingdon <kingdon@panix.com>
2018-06-30 23:43:35 -0700

    Add exmodc to iset.mm
    At least to look at it, exmo sure looks like something that should
    only hold given a decidability condition.  Certainly it is
    straightforward to show given DECID E. x ph.

--- iset.mm
+++ iset.mm
@@ -1,4 +1,4 @@
-$( iset.mm - Version of 25-Jun-2018
+$( iset.mm - Version of 30-Jun-2018
 
 Created by Mario Carneiro, starting from the 21-Jan-2015 version of
 set.mm
@@ -15014,6 +15014,12 @@ $)
   moabs $p |- ( E* x ph <-> ( E. x ph -> E* x ph ) ) $=
     ( wex weu wi wmo pm5.4 df-mo imbi2i 3bitr4ri ) ABCZKABDZEZEMKABFZENKLGNMKAB
     HZIOJ $.
+
+  $( If existence is decidable, something exists or at most one exists.
+     (Contributed by Jim Kingdon, 30-Jun-2018.) $)
+  exmodc $p |- ( DECID E. x ph -> ( E. x ph \/ E* x ph ) ) $=
+    ( wex wdc wn wo wmo df-dc weu wi pm2.21 df-mo sylibr orim2i sylbi
+    ) ABCZDPPEZFPABGZFPHQRPQPABIZJRPSKABLMNO $.
 
   ${
     $d x y $.
