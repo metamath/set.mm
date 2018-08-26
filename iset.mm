9fd355c32ceba5247bf6bba578edf5501aa261be
Jim Kingdon <kingdon@panix.com>
2018-08-26 09:41:02 -0700

    Add null set axiom section to iset.mm
    This is the section "Derive the Null Set Axiom" from set.mm,
    verbatim. Follow up commits will be required to get it to
    verify.

--- iset.mm
+++ iset.mm
@@ -32425,6 +32425,143 @@ $)
   $}
 
 $(
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+              Derive the Null Set Axiom
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+$)
+
+  ${
+    $d x y $.
+    zfnuleu.1 $e |- E. x A. y -. y e. x $.
+    $( Show the uniqueness of the empty set (using the Axiom of Extensionality
+       via ~ bm1.1 to strengthen the hypothesis in the form of ~ axnul ).
+       (Contributed by NM, 22-Dec-2007.) $)
+    zfnuleu $p |- E! x A. y -. y e. x $=
+      ( wel wn wal weu wfal wb wex nbfal albii exbii mpbi nfv bm1.1 ax-mp eubii
+      mpbir ) BADZEZBFZAGTHIZBFZAGZUDAJZUEUBAJUFCUBUDAUAUCBTKLZMNHABHAOPQUBUDAU
+      GRS $.
+  $}
+
+  ${
+    $d x y z w $.
+    $( Prove ~ axnul directly from ~ ax-rep using none of the equality axioms
+       ~ ax-8 through ~ ax-15 provided we accept ~ sp as an axiom.  Replace
+       ~ sp with the obsolete ~ ax-4 to see this in 'show trace_back'.
+       (Contributed by Jeff Hoffman, 3-Feb-2008.)  (Proof shortened by Mario
+       Carneiro, 17-Nov-2016.)  (Proof modification is discouraged.)
+       (New usage is discouraged.) $)
+    axnulALT $p |- E. x A. y -. y e. x $=
+      ( vw vz cv wcel wn wal wex wfal wa wb wi ax-rep sp con2i df-ex sylibr mpg
+      wceq fal mto pm2.21i intnan nex nbn albii exbii mpbir ) BEZAEZFZGZBHZAIUL
+      CEDEFZJAHZKZCIZLZBHZAIZUPUJUKTZMZBHZAIZVACJDABCNVCVEBVDVDGZAHZGVEVGVDVFAO
+      PVDAQRUPVBUPJUAJAOUBZUCSSUNUTAUMUSBURULUQCUPUOVHUDUEUFUGUHUI $.
+
+    $( The Null Set Axiom of ZF set theory: there exists a set with no
+       elements.  Axiom of Empty Set of [Enderton] p. 18.  In some textbooks,
+       this is presented as a separate axiom; here we show it can be derived
+       from Separation ~ ax-sep .  This version of the Null Set Axiom tells us
+       that at least one empty set exists, but does not tell us that it is
+       unique - we need the Axiom of Extensionality to do that (see
+       ~ zfnuleu ).
+
+       This proof, suggested by Jeff Hoffman, uses only ~ ax-5 and ~ ax-gen
+       from predicate calculus, which are valid in "free logic" i.e. logic
+       holding in an empty domain (see Axiom A5 and Rule R2 of [LeBlanc]
+       p. 277).  Thus, our ~ ax-sep implies the existence of at least one set.
+       Note that Kunen's version of ~ ax-sep (Axiom 3 of [Kunen] p. 11) does
+       not imply the existence of a set because his is universally closed i.e.
+       prefixed with universal quantifiers to eliminate all free variables.
+       His existence is provided by a separate axiom stating ` E. x x = x `
+       (Axiom 0 of [Kunen] p. 10).
+
+       See ~ axnulALT for a proof directly from ~ ax-rep .
+
+       This theorem should not be referenced by any proof.  Instead, use
+       ~ ax-nul below so that the uses of the Null Set Axiom can be more easily
+       identified.  (Contributed by Jeff Hoffman, 3-Feb-2008.)  (Revised by NM,
+       4-Feb-2008.)  (New usage is discouraged.)
+       (Proof modification is discouraged.) $)
+    axnul $p |- E. x A. y -. y e. x $=
+      ( vz cv wn wa wb wal wex ax-sep pm3.24 intnan id mtbiri alimi eximi ax-mp
+      wcel ) BDZADRZSCDRZSSRZUBEFZFZGZBHZAITEZBHZAIUCBACJUFUHAUEUGBUETUDUCUAUBK
+      LUEMNOPQ $.
+
+    $( The Null Set Axiom of ZF set theory.  It was derived as ~ axnul above
+       and is therefore redundant, but we state it as a separate axiom here so
+       that its uses can be identified more easily.  (Contributed by NM,
+       7-Aug-2003.) $)
+    ax-nul $a |- E. x A. y -. y e. x $.
+
+    $( The Null Set Axiom of ZF set theory: the empty set exists.  Corollary
+       5.16 of [TakeutiZaring] p. 20.  For the unabbreviated version, see
+       ~ ax-nul .  (Contributed by NM, 5-Aug-1993.)  (Proof shortened by Andrew
+       Salmon, 9-Jul-2011.) $)
+    0ex $p |- (/) e. _V $=
+      ( vx vy c0 cv wceq wex wel wn wal ax-nul eq0 exbii mpbir issetri ) ACADZC
+      EZAFBAGHBIZAFABJPQABOKLMN $.
+  $}
+
+  ${
+    sseliALT.1 $e |- A C_ B $.
+    $( Membership inference from subclass relationship.  This version of
+       ~ sseli shows how we can derive it from the inference form ~ sselii
+       using the weak deduction theorem.  (Contributed by NM, 24-Aug-2018.)
+       (Proof modification is discouraged.)  (New usage is discouraged.) $)
+    sseliALT $p |- ( C e. A -> C e. B ) $=
+      ( wcel csn cif wceq biidd eleq2 eleq1 wss sseq1 sseq2 ssid keephyp3v snid
+      c0 0ex elimhyp3v sselii dedth3v ) CAEZCBEZUDCUCBRFZGZEUCCRGZUFEABCUEUERAU
+      CAUEGZHUDIBUFCJCUGUFKUHUFUGUCUHBLUHUFLZUIUEUELUHUELUIABLABCUEUERAUHBMBUFU
+      HNCUGHUIIUEUHUEMUEUFUHNRUGHUIIDUEOPUCCUHEZUJUGUHERUEERUHEZUKABCUEUERAUHCJ
+      BUFHUJICUGUHKUEUHRJUEUFHUKIRUGUHKRSQTUAUB $.
+  $}
+
+  ${
+    $d y A $.  $d y B $.  $d x y $.
+    $( The existence of proper substitution into a class.  (Contributed by NM,
+       10-Nov-2005.)  (Revised by NM, 17-Aug-2018.) $)
+    csbexg $p |- ( A. x B e. W -> [_ A / x ]_ B e. _V ) $=
+      ( vy cvv wcel wal csb wa wsbc cab df-csb abid2 elex syl5eqel alimi adantr
+      cv spsbc syl5 imp wb nfcv sbcabel mpbid wn csbprc 0ex syl6eqel pm2.61ian
+      c0 ) BFGZCDGZAHZABCIZFGZUMUOJZUPESCGZABKELZFAEBCMURUSELZFGZABKZUTFGZUMUOV
+      CUOVBAHUMVCUNVBAUNVACFECNCDOPQVBABFTUAUBUMVCVDUCUOUSAEBFFAFUDUERUFPUMUGZU
+      QUOVEUPULFABCUHUIUJRUK $.
+  $}
+
+  ${
+    csbex.1 $e |- B e. _V $.
+    $( The existence of proper substitution into a class.  (Contributed by NM,
+       7-Aug-2007.)  (Proof shortened by Andrew Salmon, 29-Jun-2011.)  (Revised
+       by NM, 17-Aug-2018.) $)
+    csbex $p |- [_ A / x ]_ B e. _V $=
+      ( cvv wcel csb csbexg mpg ) CEFABCGEFAABCEHDI $.
+  $}
+
+  ${
+    $d y A $.  $d y B $.  $d x y $.
+    $( The existence of proper substitution into a class.  (Contributed by NM,
+       10-Nov-2005.)  Use ~ csbexg instead of this.  Obsolete as of
+       17-Aug-2018.  (New usage is discouraged.)
+       (Proof modification is discouraged.) $)
+    csbexgOLD $p |- ( ( A e. V /\ A. x B e. W ) -> [_ A / x ]_ B e. _V ) $=
+      ( vy wcel wal wa csb wsbc cab cvv df-csb abid2 elex syl5eqel alimi spsbc
+      cv syl5 imp wb nfcv sbcabel adantr mpbid ) BDGZCEGZAHZIZABCJFTCGZABKFLZMA
+      FBCNUKULFLZMGZABKZUMMGZUHUJUPUJUOAHUHUPUIUOAUIUNCMFCOCEPQRUOABDSUAUBUHUPU
+      QUCUJULAFBMDAMUDUEUFUGQ $.
+  $}
+
+  ${
+    csbexOLD.1 $e |- A e. _V $.
+    csbexOLD.2 $e |- B e. _V $.
+    $( The existence of proper substitution into a class.  (Contributed by NM,
+       7-Aug-2007.)  (Proof shortened by Andrew Salmon, 29-Jun-2011.)  Use
+       ~ csbex instead of this.  Obsolete as of 17-Aug-2018.
+       (New usage is discouraged.)  (Proof modification is discouraged.) $)
+    csbexOLD $p |- [_ A / x ]_ B e. _V $=
+      ( cvv wcel csb wal csbexgOLD mpan mpg ) CFGZABCHFGZABFGMAINDABCFFJKEL $.
+  $}
+
+
+$(
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
        Appendix:  Typesetting definitions for the tokens in this file
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
