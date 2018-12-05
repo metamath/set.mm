3bc612a5350b90a6499d2a97ec9f5d415d52f23a
David A. Wheeler <dwheeler@dwheeler.com>
2018-12-05 10:33:40 -0500

    Fix date at top of iset.mm
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- iset.mm
+++ iset.mm
@@ -1,4 +1,4 @@
-$( iset.mm - Version of 1-Dec-2018
+$( iset.mm - Version of 5-Dec-2018
 
 Created by Mario Carneiro, starting from the 21-Jan-2015 version of
 set.mm (with updates since then, including copying entire theorems
