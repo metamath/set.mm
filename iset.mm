10bd65fcc15fcca22dfff115a2be744263631ec2
Jim Kingdon <kingdon@panix.com>
2019-09-17 07:50:23 -0700

    Add mulcomnqg to iset.mm
    This is based on mulcompq in 2012 set.mm, intuitionized by changing
    it to use ecovicom and leaving out the case elimination.

--- iset.mm
+++ iset.mm
@@ -1,4 +1,4 @@
-$( iset.mm - Version of 16-Sep-2019
+$( iset.mm - Version of 17-Sep-2019
 
 Created by Mario Carneiro, starting from the 21-Jan-2015 version of
 set.mm (with updates since then, including copying entire theorems
@@ -58597,6 +58597,17 @@ $)
       QYTYRVNXFXIXOWNYIQZXPXFXHXOUVDXGXEXHXOUVDXDWGWBWMTVOVPVQVKYFXAYKWQIYFXEYA
       YBXAYKQYSUUAUUBWGWRWSVEVDVRVSXFXIXPWPXCQZXOXFXHXPUVEXGXEXHXPUVEXDWGWBWCTV
       OVPVTWA $.
+  $}
+
+  ${
+    $d x y z w A $.  $d x y z w B $.
+    $( Multiplication of positive fractions is commutative.  (Contributed by
+       Jim Kingdon, 17-Sep-2019.) $)
+    mulcomnqg $p |- ( ( A e. Q. /\ B e. Q. ) -> ( A .Q B ) = ( B .Q A ) ) $=
+      ( vx vy vz vw cnq cv cmi co cmq ceq cnpi df-nqqs mulpipqqs wcel mulcompig
+      wceq ad2ant2r ad2ant2l ecovicom ) CDEFABGCHZEHZIJZKLMDHZFHZIJZUCUBIJZUFUE
+      IJZNUBUEUCUFOUCUFUBUEOUBMPZUCMPZUDUHRUEMPZUFMPZUBUCQSULUMUGUIRUJUKUEUFQTU
+      A $.
   $}
 
 $(
