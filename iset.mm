b1aeb48fe10cdf6ac72925068c742ce505f41e77
Jim Kingdon <kingdon@panix.com>
2019-09-19 19:23:25 -0700

    Modify df-rq in iset.mm
    This is based on df-rq from the 2012 set.mm with the y e. Q.
    condition added. My guess is that this is equivalent to the
    one without the y e. Q. condition, or the df-rq from
    2019 set.mm, but I wasn't able to quickly prove that, so at
    least for now adopt a definition which is (at least for
    current uses) easier to work with.

--- iset.mm
+++ iset.mm
@@ -1,4 +1,4 @@
-$( iset.mm - Version of 19-Sep-2019
+$( iset.mm - Version of 20-Sep-2019
 
 Created by Mario Carneiro, starting from the 21-Jan-2015 version of
 set.mm (with updates since then, including copying entire theorems
@@ -58234,9 +58234,10 @@ $)
        will never need it).  This is a "temporary" set used in the construction
        of complex numbers, and is intended to be used only by the
        construction.  From Proposition 9-2.5 of [Gleason] p. 119, who uses an
-       asterisk to denote this unary operation.  (Contributed by NM,
-       6-Mar-1996.)  (New usage is discouraged.) $)
-    df-rq $a |- *Q = ( `' .Q " { 1Q } ) $.
+       asterisk to denote this unary operation.  (Contributed by Jim Kingdon,
+       20-Sep-2019.)  (New usage is discouraged.) $)
+    df-rq $a |- *Q = { <. x , y >. |
+        ( x e. Q. /\ y e. Q. /\ ( x .Q y ) = 1Q ) } $.
 
     $( Define ordering relation on positive fractions.  This is a "temporary"
        set used in the construction of complex numbers, and is intended to be
