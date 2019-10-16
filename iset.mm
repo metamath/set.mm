c38c033a3637d55874c1af04787a5955f148ecf3
Benoit <benoit.jubin@gmail.com>
2019-10-17 01:43:47 +0200

    typo

--- iset.mm
+++ iset.mm
@@ -1,4 +1,4 @@
-$( iset.mm - Version of 15-Oct-2019
+$( iset.mm - Version of 16-Oct-2019
 
 Created by Mario Carneiro, starting from the 21-Jan-2015 version of
 set.mm (with updates since then, including copying entire theorems
