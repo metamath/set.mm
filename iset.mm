733aad6c018c4ae455ca66e3886c8c7979b9448a
Benoit Jubin <benoit.jubin@gmail.com>
2020-01-28 01:28:10 +0100

    fix top date

--- iset.mm
+++ iset.mm
@@ -1,4 +1,4 @@
-$( iset.mm - Version of 26-Jan-2020
+$( iset.mm - Version of 27-Jan-2020
 
 Created by Mario Carneiro, starting from the 21-Jan-2015 version of
 set.mm (with updates since then, including copying entire theorems
