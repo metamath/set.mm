b40bde7cc6e9483534adb7f3088799c6dcef2a96
Jim Kingdon <kingdon@panix.com>
2023-04-22 13:17:26 -0700

    Update contact info
    Remove Norm's email a few places and tweak language in mm_100 which
    already sort of points people to github but can do so a bit more
    clearly.

--- miu.mm
+++ miu.mm
@@ -6,7 +6,7 @@ $(
 This work is waived of all rights, including copyright, according to the CC0
 Public Domain Dedication.  http://creativecommons.org/publicdomain/zero/1.0/
 
-Norman Megill - email: nm at alum.mit.edu
+Norman Megill
 
 $)
 
