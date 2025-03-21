b89095eb776c3988ebc614eda57d88c792f74e66
Jim Kingdon <kingdon@panix.com>
2025-03-20 23:38:38 -0700

    copy homid from set.mm to iset.mm

--- iset.mm
+++ iset.mm
@@ -144078,6 +144078,11 @@ $)
     UMJKVMFVKWAUNLMNVPOWACOWAUOEFOPHUPVDJKVOOVKWAUQLMNURVRVTVREQGZWACWBWAUSEQFR
     UTSVATKVQWBVKWAVBLMNVTEDGZWACWCWAWCEDRVCVEVFEDFRISVGTKVSWCVKWAVHLMNVIVI $.
 
+  $( Utility theorem: index-independent form of ~ df-hom .  (Contributed by
+     Mario Carneiro, 7-Jan-2017.) $)
+  homid $p |- Hom = Slot ( Hom ` ndx ) $=
+    ( chom c1 c4 cdc df-hom 1nn0 4nn decnncl ndxid ) ABCDEBCFGHI $.
+
 
 $(
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
