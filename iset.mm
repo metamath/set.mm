ddbe473484c7eb42cb28b1a5e15366c0b2c98348
Jim Kingdon <kingdon@panix.com>
2025-06-30 21:16:29 -0700

    Add igsumval to iset.mm
    This is gsumval in set.mm but adjusted for the differences between
    gsum definitions.  The proof is similar to the set.mm proof.

--- iset.mm
+++ iset.mm
@@ -147211,6 +147211,17 @@ $)
         DXEUUDUUGWPXFUVLUVNUUGXGXHXIXJUVLUVOGXKXHUVJUVPUVLUUGGWRXAXLXMXIUVJFWRX
         AXMWQXNUVDUVESSXOXPWNUUKBSXQWMXR $.
     $}
+
+    $d .+ x $.  $d .0. x $.  $d F m n x $.  $d G m n x $.  $d m n ph x $.
+    gsumval.a $e |- ( ph -> A e. X ) $.
+    gsumval.f $e |- ( ph -> F : A --> B ) $.
+    $( Expand out the substitutions in ~ df-igsum .  (Contributed by Mario
+       Carneiro, 7-Dec-2014.) $)
+    igsumval $p |- ( ph -> ( G gsum F ) =
+         ( iota x ( ( A = (/) /\ x = .0. )
+         \/ E. m E. n e. ( ZZ>= ` m )
+            ( A = ( m ... n ) /\ x = ( seq m ( .+ , F ) ` n ) ) ) ) ) $=
+      ( cvv fexd fdmd igsumvalx ) ABCDEFGHIJSLMNOPACDKHRQTACDHRUAUB $.
   $}
 
 
