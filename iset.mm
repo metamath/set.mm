fc45f6edcc795c286d60bd8fb550360b52759746
Jim Kingdon <kingdon@panix.com>
2026-04-02 16:59:14 -0700

    Add gfsumsn to iset.mm
    This is gsumsn from set.mm but for gfsum rather than gsum .  The proof
    is via gsumfzsnfd and gfsumval .

--- iset.mm
+++ iset.mm
@@ -209238,6 +209238,27 @@ $)
       BVNXC $.
   $}
 
+  ${
+    $d x A $.  $d k x B $.  $d k x C $.  $d k x G $.  $d k x M $.  $d k x V $.
+    gfsumsn.b $e |- B = ( Base ` G ) $.
+    gfsumsn.s $e |- ( k = M -> A = C ) $.
+    $( Group sum of a singleton.  (Contributed by Jim Kingdon, 2-Apr-2026.) $)
+    gfsumsn $p |- ( ( G e. CMnd /\ M e. V /\ C e. B ) ->
+        ( G gfsum ( k e. { M } |-> A ) ) = C ) $=
+      ( vx wcel csn cmpt co c1 cgsu wceq 3ad2ant2 cfz cz ccmn w3a cgfsu ccom cv
+      simp1 elsni syl adantl simpl3 eqeltrd fmpttd cfn snfig chash cfv wf1o cop
+      wa 1z simp2 f1osng sylancr fmptsn eqcomd hashsng oveq2d fzsn ax-mp eqtrdi
+      eqidd f1oeq123d mpbird gfsumval snidg adantr cofmpt eqid fvmptd3 mpteq2dv
+      simp3 eqtrd cmnmndd 1zzd nfv nfcv gsumfzsnfd 3eqtrd ) EUAKZFGKZCBKZUBZEDF
+      LZAMZUCNEWNJOLZFMZUDZPNEJWOCMZPNCWLWMBWNWPEHWIWJWKUFZWLDWMABWLDUEZWMKZUSA
+      CBXAACQZWLXAWTFQXBWTFUGIUHUIWIWJWKXAUJUKULZWJWIWMUMKWKFGUNRWLOWMUOUPZSNZW
+      MWPUQWOWMOFURLZUQZWLOTKZWJXGUTWIWJWKVAZOFTGVBVCWLXEWOWMWMWPXFWLXFWPWLXHWJ
+      XFWPQUTXIJOFTGVDVCVEWJWIXEWOQWKWJXEOOSNZWOWJXDOOSFGVFVGXHXJWOQUTOVHVIVJRW
+      LWMVKVLVMVNWLWQWREPWLWQJWOFWNUPZMWRWLJWOFWMBWNXCWLFWMKZJUEZWOKWJWIXLWKFGV
+      ORZVPVQWLJWOXKCWLDFACWMWNBWNVRIXNWIWJWKWAZVSVTWBVGWLCBCJEOHWLEWSWCWLWDXOW
+      LXMOQUSCVKWLJWEJCWFWGWH $.
+  $}
+
 $( (End of Jim Kingdon's mathbox.) $)
 
 
